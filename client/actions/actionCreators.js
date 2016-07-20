7060ce2e608a7eac1bd671d8a64445d0f9c8a4e3
matt-oconnell <mattoconnell408@gmail.com>
2016-07-20 10:02:26 -0400

    remove backups

--- client/actions/actionCreators.js
+++ client/actions/actionCreators.js
@@ -1,10 +1,8 @@
 import $ from 'jquery'
 
 // these are "action creators." they put together the action and dispatch it
-export function submitAnswer(answer, question) {
-  	const q = question
-  	const correctAnswer = q.answers[q.correctAnswerIdx]
-  	const correct = correctAnswer === answer
+export function submitAnswer(userAnswer, correctAnswer) {
+  	const correct = correctAnswer === userAnswer
   	return {
     		type: 'SUBMIT_ANSWER',
     		correct
